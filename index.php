6e2b6a8d5918f39a804012199f9fdb8fb04b706d
Matheus Leite <matheussantosleite_@hotmail.com>
2018-07-29 17:41:24 -0300

    Modificando item na pagina Cadastro

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $arrayProdutos = $comando->listar();
 <body>
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">Projeto Cadastro</a>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+
 
 </nav>
 
